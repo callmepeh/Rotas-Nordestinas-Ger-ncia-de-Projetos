60727be5a639ce085e6c3144e48a673d022f6672
Pedro Henrique Carvalho <pedrosasada@gmail.com>
2025-11-10 13:41:45 -0300

    Refactor cidadesRoutes to use controller methods

--- backend/src/routes/cidadesRoutes.js
+++ backend/src/routes/cidadesRoutes.js
@@ -1,22 +1,9 @@
-const express = require('express');
-const supabase = require('../database/supabaseClient.js');
-
+const express = require("express");
 const router = express.Router();
+const cidadesController = require("../controllers/cidadesController");
 
-router.get("/", async (req, res) => {
-    const { data, error } = await supabase
-        .from("Cidades")              // nome da tabela
-        .select("*")
-        .order("nomeCidade", { ascending: true });  // coluna certa
-
-    if (error) {
-        console.error("ERRO SUPABASE:", error);
-        return res.status(500).json({ error: "Erro ao buscar cidades" });
-    }
-
-    return res.status(200).json(data);
-});
-
-
+router.get("/buscar", cidadesController.buscarPorNome);
+router.get("/estado/:estadoID", cidadesController.listarPorEstado);
+router.get("/:id", cidadesController.buscarPorId);
 
 module.exports = router;
